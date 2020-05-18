3221395caa58c927908f2f35db8b66b23a0d3555
Arafath1019 <arafath.yeasin1019@gmail.com>
2020-05-19 03:26:26 +0600

    Uplaoding Big O Analysis and Scalability file

---
+++ Big O in JavaScript/3. Big O analysis and scalibility.js
@@ -0,0 +1,32 @@
+// Big O Analysis and Scalability:
+
+/*
+    Suppose, one day after a log lists of attempts, i had made a program that can perform a task with 1000 inputs, and my program was took only 3.5 millisecond, so as obviously, i had not learnt Data Structure and Algorithm yet, so i was super super excited as i was created a program which could perform with 1000inputs within 3.5millisecond.
+
+    Then, one day i told one of my friend who was also a student like me, i shared my program with him, and he told me to give my code to him, and i gave him. Then run my program  in his computer and had informed me a socking news that in his computer my program took only 1.5 millisecond. 
+    
+    Holly molly, i was so surprised to hear this, before this i had believed that only my code optimization and good and simple logic is responsible for my program performance. Though, my friend's computer was more faster than me. 
+    
+    Then both of us was trying to find out the actual reason why my program executes in different time in different computer.
+
+    After a little bit searching in the google, we have found our new knowledge that, a program's run time depends not only programs logic or other stuff's, it also depends on the computer where it is run, which cpu it is used, which other programs are run besides this or which programming language is being used. 
+
+    Then a thing was around in our head that, if i use a super fast computer, then my program will be super fast also. But isn't it a funny concept while we are learning programing to run a program in a better way. 
+
+    Then we had introduced with a super super crazy topics which is Big O analysis or Big O notation. Before introducing this, we have to learn with what is scalability?
+
+    Scalability means in our program, in every steps how much time it takes to run our program. This is called scalability.
+
+    To define this scalability concept, there comes Big O Notations or Big O analysis. Which define how much time it will take in a program to execute a single portion of our program of whole program.
+
+
+    For defining Big O notations, there is a chart called Big O Complexity chart.
+
+    Big O complexity chart is a chart of no of operations vs no of inputs/ elements. Using this chart we can assume the efficiency of a program or how much time it will take to execute. 
+
+    Sequence of efficiency according to Big O analysis is
+         
+        O(log n), O(1) ----> O(n) ---> O(n log n) ----> O(n^2) ----> O(2^n) ----> O(n!)
+
+    This is the series of our program efficiency according our Big O analysis chart, where O(n!) is the most horrible case and O(log n)/ O(1) is the most excellent case for a program execution case.
+*/
