5d5825fe31e9bb916c7bd132033d3b6b9cd39896
Arafath1019 <arafath.yeasin1019@gmail.com>
2020-05-19 01:56:43 +0600

    Uploading set up environment and what is good code

---
+++ Big O in JavaScript/1. Setting Up Environment.js
@@ -0,0 +1,7 @@
+//Setting Up Environment for this Exercise:
+
+/*
+    1. Using browser console to run the js code.
+    2. Using Text editor to run the js code, this time you must have set up NodeJs in your local machine to run js file from your terminal or cmd.
+    3. Using online editor like repl.it or glot.io etc.
+*/
---
+++ Big O in JavaScript/2. What is Good Code.js
@@ -0,0 +1,27 @@
+//What is Good Code?
+
+/*
+    Before knowing about the good code, we must have to learn about what is code?
+    And the answer of this question is, in a general sense, code is a language that computer can understand. As we all know that computer can't understand natural language,
+    so we need some especial things to understand the computer that what are we going to do. In this case, code is a set of instructions that performs task/tasks which is written in some special words is called code. And this special words are called programming language constructs.
+    
+    
+    Now, we all know that what is code. Now we can jump into our topic. And our topic is what is good code. 
+
+    Good code can be described in two things:
+        1. Readable 
+        2. Scalable
+    
+    
+    Good code must have the following properties:
+        1. Maintainability: If it can be understood, maintained and extended by any other developer, then this code can obviously called good code.
+        2. Efficiency
+        3. Elegance: simple, proper use of language constructs and environment compatibilities.
+        4. Modularity 
+        5. Proper Object Oriented Design.
+        6. Simplicity
+        7. Layering
+        8. Design
+        9. Commenting
+        etc.
+*/
