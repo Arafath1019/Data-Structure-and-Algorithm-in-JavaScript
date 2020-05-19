afb11db51ef8d84ce016eeb620b22c43137e59b4
Arafath1019 <arafath.yeasin1019@gmail.com>
2020-05-20 03:05:59 +0600

    Uploading Big O cheat sheet, pillars of programming, Space complexity file

---
+++ Big O in JavaScript/15. Pillars of Programming.js
@@ -0,0 +1,15 @@
+// Pillars of Programming:
+
+/* Now at this situation, we can say that a good code must have two properties: 
+    1. Readable
+    2. Scalable
+
+But there are two parts of Scalable properties. And they are 
+    1. Speed / Time Complexity
+    2. Memory / Space Complexity
+*/
+
+// So, Pillars of our programming are : 
+    // 1. Readable
+    // 2. Speed / Time Complexity
+    // 3. Memory / Time Complexity
---
+++ Big O in JavaScript/16. Space complexity.js
@@ -0,0 +1,19 @@
+// What is Space Complexity?
+
+/*
+    Space complexity of an algorithm is the total space taken by the algorithm with respect to the input size.
+
+    Auxiliary space is the extra space or temporary  space used by an  algorithm.
+
+    Space complexity includes both auxiliary space and space used by input.
+
+*/
+
+// What causes space complexity?
+
+/*
+    1. adding variables
+    2. adding data structures
+    3. Function call
+    4. Allocations
+*/
