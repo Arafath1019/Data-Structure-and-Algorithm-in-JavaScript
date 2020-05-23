7d53a0e47bae1d552f1bb61e5ab5fd06cca01f8e
Arafath1019 <arafath.yeasin1019@gmail.com>
2020-05-24 04:20:35 +0600

    First recurring function

--- 6. Data Structures Hash Tables/5. First Recurring Character.js
+++ 6. Data Structures Hash Tables/5. First Recurring Character.js
@@ -16,6 +16,7 @@
 
 //First Method / O(n^2)
 
+
 function firstRecurringCharacter1(input){
     for(let i = 0; i < input.length; i++){
         for(let j = i + 1; j < input.length; j++){
@@ -48,4 +49,3 @@ function firstRecurringCharacter2(input){
 }
 
 firstRecurringCharacter2([2,5,1,2,3,5,1,2,4]);
-
